778bf9cf48028937989224975fa5a3ae382e10b7
jorgeav527 <jorgeav527@gmail.com>
2026-07-17 21:59:21 -0500

    feat: first end point

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sqlite3
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,10 +12,24 @@ app = Flask(__name__)
 
 def get_db():
     db = sqlite3.connect(DB_URL)
-    db.row_factory = sqlite3.Row
+    # db.row_factory = sqlite3.Row
     return db
 
+@app.route("/", methods=['GET'])
+def home():
+    return render_template("base.html")
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+@app.route("/posts", methods=['GET'])
+def list_posts():
+    connection = get_db()
+    posts_data = connection.execute("SELECT * FROM posts").fetchall()
+    connection.close()
+    return render_template("index.html", posts_list=posts_data)
+
+
+@app.route("/api/posts", methods=['GET'])
+def list_posts_json():
+    connection = get_db()
+    posts_data = connection.execute("SELECT * FROM posts").fetchall()
+    connection.close()
+    return jsonify(posts_data)
